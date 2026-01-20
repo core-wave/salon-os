f24886c573ed75774397147f5b758025294907ea
David Oost <david@newblack.io>
2026-01-20 13:36:24 +0100

    env validation

--- lib/env/client.ts
+++ lib/env/client.ts
@@ -1,3 +1,16 @@
+import { ZodError } from "zod";
 import { clientSchema } from "./schema";
 
-export const clientEnv = clientSchema.parse(process.env);
+export const clientEnv = (() => {
+  try {
+    return clientSchema.parse(process.env);
+  } catch (err) {
+    if (err instanceof ZodError) {
+      const vars = err.issues.map((issue) => `- ${issue.path.join(".")}`);
+
+      throw new Error(`Missing or invalid env vars:\n${vars.join("\n")}`);
+    }
+
+    throw err;
+  }
+})();
--- lib/env/server.ts
+++ lib/env/server.ts
@@ -1,3 +1,16 @@
+import { ZodError } from "zod";
 import { serverSchema } from "./schema";
 
-export const serverEnv = serverSchema.parse(process.env);
+export const serverEnv = (() => {
+  try {
+    return serverSchema.parse(process.env);
+  } catch (err) {
+    if (err instanceof ZodError) {
+      const vars = err.issues.map((issue) => `- ${issue.path.join(".")}`);
+
+      throw new Error(`Missing or invalid env vars:\n${vars.join("\n")}`);
+    }
+
+    throw err;
+  }
+})();
